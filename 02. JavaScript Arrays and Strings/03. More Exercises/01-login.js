45373818b5ede10525d11fa60bf4e8b75acd5db0
Dimitar-Peev <mctrix87@abv.bg>
2024-07-03 07:15:46 +0300

    Create 01-login.js

--- 02. JavaScript Arrays and Strings/03. More Exercises/01-login.js
+++ 02. JavaScript Arrays and Strings/03. More Exercises/01-login.js
@@ -1,20 +1,20 @@
 function login(string) {
-	const username = string[0];
-	const password = username.split("").reverse().join("");
+    const username = string[0];
+    const password = username.split("").reverse().join("");
 
-	for (let i = 1; i < string.length; i++) {
-		if (string[i] === password) {
-			console.log(`User ${username} logged in.`);
-			break;
-		} else {
-			if (i === 4) {
-				console.log(`User ${username} blocked!`);
-				break;
-			} else {
-				console.log("Incorrect password. Try again.");
-			}
-		}
-	}
+    for (let i = 1; i < string.length; i++) {
+        if (string[i] === password) {
+            console.log(`User ${username} logged in.`);
+            break;
+        } else {
+            if (i === 4) {
+                console.log(`User ${username} blocked!`);
+                break;
+            } else {
+                console.log("Incorrect password. Try again.");
+            }
+        }
+    }
 }
 
 login(["Acer", "login", "go", "let me in", "recA"]);
