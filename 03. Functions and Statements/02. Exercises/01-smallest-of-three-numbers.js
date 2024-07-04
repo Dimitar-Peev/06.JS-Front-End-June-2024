12714b8a3d01caa41a5816f7218c81fb9cc21e00
Dimitar-Peev <mctrix87@abv.bg>
2024-07-05 00:13:47 +0300

    Create 01-smallest-of-three-numbers.js

---
+++ 03. Functions and Statements/02. Exercises/01-smallest-of-three-numbers.js
@@ -0,0 +1,8 @@
+function findSmallest(n1, n2, n3) {
+	console.log(Math.min(n1, n2, n3));
+}
+
+findSmallest(2, 5, 3); // 2
+findSmallest(600, 342, 123); // 123
+findSmallest(25, 21, 4); // 4
+findSmallest(2, 2, 2); // 2
