a41aa115c54ff368cd4cadac50d16b1eb4e5638a
Dimitar-Peev <mctrix87@abv.bg>
2024-07-06 09:00:00 +0300

    Create 07-NxN-matrix.js

---
+++ 03. Functions and Statements/02. Exercises/07-NxN-matrix.js
@@ -0,0 +1,18 @@
+function printMatrix(n) {
+	for (let i = 0; i < n; i++) {
+		console.log(`${n} `.repeat(n));
+	}
+}
+
+printMatrix(3);
+// 3 3 3
+// 3 3 3
+// 3 3 3
+printMatrix(7);
+// 7 7 7 7 7 7 7
+// 7 7 7 7 7 7 7
+// 7 7 7 7 7 7 7
+// 7 7 7 7 7 7 7
+// 7 7 7 7 7 7 7
+// 7 7 7 7 7 7 7
+// 7 7 7 7 7 7 7
