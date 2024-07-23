c8288e1caffd15612014cfe97630405dcbb28fff
Dimitar-Peev <mctrix87@abv.bg>
2024-07-23 20:23:53 +0300

    Create "03-points-validation.js"

---
+++ 03. Functions and Statements/03. More Exercises/03-points-validation.js
@@ -0,0 +1,33 @@
+function checkDistanceValidity(inputArr) {
+    const [x1, y1, x2, y2] = inputArr;
+    function calculateDistance(xA, yA, xB, yB) {
+        return Math.sqrt((xA - xB) ** 2 + (yA - yB) ** 2);
+    }
+
+    if (Number.isInteger(calculateDistance(x1, y1, 0, 0))) {
+        console.log(`{${x1}, ${y1}} to {0, 0} is valid`);
+    } else {
+        console.log(`{${x1}, ${y1}} to {0, 0} is invalid`);
+    }
+
+    if (Number.isInteger(calculateDistance(x2, y2, 0, 0))) {
+        console.log(`{${x2}, ${y2}} to {0, 0} is valid`);
+    } else {
+        console.log(`{${x2}, ${y2}} to {0, 0} is invalid`);
+    }
+
+    if (Number.isInteger(calculateDistance(x1, y1, x2, y2))) {
+        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is valid`);
+    } else {
+        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is invalid`);
+    }
+}
+
+checkDistanceValidity([3, 0, 0, 4]);
+// {3, 0} to {0, 0} is valid
+// {0, 4} to {0, 0} is valid
+// {3, 0} to {0, 4} is valid
+checkDistanceValidity([2, 1, 1, 1]);
+// {2, 1} to {0, 0} is invalid
+// {1, 1} to {0, 0} is invalid
+// {2, 1} to {1, 1} is valid
