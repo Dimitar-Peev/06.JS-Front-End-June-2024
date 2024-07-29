ae4cd11aac420bc0053382497d6e1808ed632a61
Dimitar-Peev <mctrix87@abv.bg>
2024-07-29 13:27:53 +0300

    Create "02-catalogue.js"

---
+++ 04. Objects and Classes/03. More Exercises/02-catalogue.js
@@ -0,0 +1,40 @@
+function createSortedCatalog(productsList) {
+    const catalogue = {};
+
+    for (const product of productsList) {
+        const [productName, price] = product.split(" : ");
+
+        if (!catalogue[productName[0]]) {
+            catalogue[productName[0]] = [];
+        }
+
+        catalogue[productName[0]].push({
+            name: productName,
+            price: Number(price),
+        });
+    }
+
+    Object.entries(catalogue)
+        .sort((a, b) => a[0].localeCompare(b[0]))
+        .forEach(([key, value]) => {
+            console.log(key);
+            value
+                .sort((a, b) => a.name.localeCompare(b.name))
+                .forEach(({name, price}) =>
+                    console.log(`  ${name}: ${price}`)
+                );
+        });
+}
+
+createSortedCatalog([
+    "Appricot : 20.4",
+    "Fridge : 1500",
+    "TV : 1499",
+    "Deodorant : 10",
+    "Boiler : 300",
+    "Apple : 1.25",
+    "Anti-Bug Spray : 15",
+    "T-Shirt : 10",
+]);
+
+createSortedCatalog(["Omlet : 5.4", "Shirt : 15", "Cake : 59"]);
